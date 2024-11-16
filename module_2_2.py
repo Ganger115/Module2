dcce32c977dd5caf4872525bb66abc05fa7eb0f8
Алексей <108700816+Ganger115@users.noreply.github.com>
2024-11-16 20:19:32 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -1,32 +1,13 @@
-print("Ввод в консоль 1:") #Первая консоль
+console_output = "Вывод на консоль 1:"
+print("Ввод в консоль 1:")
 
 first = input()
 second = input()
 third  = input()
 
 if first == second and second == third:
-    print("Вывод на консоль 1:", """
-3""")
+    print(console_output, "\n3")
 elif first == second or second == third or first == third:
-    print("Вывод на консоль 1:", """
-2""")
+    print(console_output, "\n2")
 else:
-    print("Вывод на консоль 1:", """
-0""")
-
-print("""
-Ввод в консоль 2:""") #Вторая консоль
-
-first = input()
-second = input()
-third = input()
-
-if first == second and second == third:
-    print("Вывод на консоль 2:", """
-3""")
-elif first == second or second == third or first == third:
-    print("Вывод на консоль 2:", """
-2""")
-else:
-    print("Вывод на консоль 2:", """
-0""")
+    print(console_output, "\n0")
